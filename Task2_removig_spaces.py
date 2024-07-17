7b995a136b66d0a581bacd43595240e6e5dc44cc
Adarsh <adarshuppuleti@gmail.com>
2024-07-17 10:28:47 +0530

    Second

---
+++ Task2_removig_spaces.py
@@ -0,0 +1,25 @@
+# by using replace
+a = "How Are You Adarsh Are You Ok ?"
+b = a.replace(" ",'')
+
+print(b)  # the out put is : HowAreYouAdarshAreYouOk?
+#------------------------------------------------------------------
+#by split and join
+a = "How Are You Adarsh Are You Ok ?"
+b = a.split(" ")
+c = "".join(b)
+print(c)
+
+#---------------------------------------------------------
+a = "How Are You Adarsh Are You Ok ?"
+b = ''
+for i in a:
+    if i !=" ":
+        b = b+i
+print(b)
+ 
+
+
+
+
+
